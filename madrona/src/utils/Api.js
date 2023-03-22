d08de3f1cd95eb85102edb9e0ee5b31aaa4739a7
tom <t_hankinson@outlook.com>
2023-03-22 09:57:00 +0000

    update

--- madrona/src/utils/Api.js
+++ madrona/src/utils/Api.js
@@ -9,7 +9,7 @@ export default class SearchLocation {
       console.log('API hit')
       console.log('Eviroment variables ', REACT_APP_API_KEY, REACT_APP_API_URL)
       console.log(this.search)
-      const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=fb611d93a6ed406588e85145231703&q=${this.search}`)
+      const response = await fetch(`${this.search}`)
       const data = await response.json()
       console.log(data)
       return data
